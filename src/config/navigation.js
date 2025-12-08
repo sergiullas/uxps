54a2f36636d99e469284b7ee0777b07d5e716f9e
Sergiullas <sergiullas@gmail.com>
2025-12-07 20:08:02 -0500

    Simplify navigation config structure

--- src/config/navigation.js
+++ src/config/navigation.js
@@ -4,25 +4,17 @@ export const navigation = [
   {
     id: 'work',
     label: 'Work',
-    href: '#work', // will be refined later for scroll behavior
-    sectionId: 'work',
   },
   {
     id: 'about',
     label: 'About',
-    href: '#about',
-    sectionId: 'about',
   },
   {
     id: 'resume',
     label: 'Resume',
-    href: '#resume',
-    sectionId: 'resume',
   },
   {
     id: 'contact',
     label: 'Contact',
-    href: '#contact',
-    sectionId: 'contact',
   },
 ];
